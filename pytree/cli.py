9633f4412c09375fe3e0538e85f69126320bbcaf
ADU <LukeDu571@gmail.com>
2018-08-06 15:17:10 +1000

    finish commandline

---
+++ pytree/cli.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+# !/usr/bin env python3
+"""list contents of directories in a tree-like format.
+  Usage:
+    pytree <dir>
+    pytree -h | --help | --version
+"""
+import pytree.core as pytree
+import pytree.__version__ as version
+
+
+def main():
+    from docopt import docopt
+    arguments = docopt(__doc__, version=version.__version__)
+    dir_name = arguments['<dir>']
+    print('\n'.join(pytree.render_tree(pytree.tree_format('', dir_name))))
+
+
+if __name__ == "__main__":
+    main()
